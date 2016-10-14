2b55aa7b64fc2e429cc88db2dcc2dbfa89270194
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-14 15:35:06 +0200

    Test, this probably doesnt work

--- src/Pckg/Migration/Constraint/Index.php
+++ src/Pckg/Migration/Constraint/Index.php
@@ -7,4 +7,14 @@ class Index extends Constraint
 
     protected $type = 'KEY';
 
+    public function getSql()
+    {
+        return $this->type . '(`' . implode('`,`', $this->fields) . '`)';
+    }
+
+    public function getName()
+    {
+        return 'INDEX';
+    }
+
 }
