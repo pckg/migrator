9ca378c85bb593126e26a11061571b198955ef7e
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-02-06 11:03:29 +0100

    Translations helper

---
+++ src/Pckg/Migration/Helper/Translations.php
@@ -0,0 +1,33 @@
+<?php namespace Pckg\Migration\Helper;
+
+use Pckg\Generic\Record\ListRecord;
+use Pckg\Translator\Record\Translation;
+
+/**
+ * Class ListItem
+ *
+ * @package Pckg\Migration\Helper
+ */
+trait Translations
+{
+
+    /**
+     * @param $id
+     *
+     * @return ListRecord
+     */
+    protected function translation($slug, $translations = [])
+    {
+        if (!is_array($translations)) {
+            $translations['en'] = $translations;
+        }
+
+        $t = Translation::getOrCreate(['slug' => $slug]);
+        foreach ($translations as $language => $translation) {
+            $t->value = $translation;
+            $t->language_id = $language;
+            $t->save();
+        }
+    }
+
+}
